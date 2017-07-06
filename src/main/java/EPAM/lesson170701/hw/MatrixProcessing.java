5d16bd8f11b4a61620b13f661b8fc0408b9c30f2
loryrosh <sasharo@mail.ru>
2017-07-06 11:45:43 +0300

    hw - counting sums for 2D matrix - update

--- src/main/java/EPAM/lesson170701/hw/MatrixProcessing.java
+++ src/main/java/EPAM/lesson170701/hw/MatrixProcessing.java
@@ -5,35 +5,13 @@ public class MatrixProcessing {
         int[][] matrix = {
                 {0, 1, 2},
                 {3, 4, 5},
-                {6, 7, 8}
+                {6, -7, 8}
         };
 
-        int k = count(matrix, 10);
-        int sum = countSumInRow(matrix, 1);
+        //int sum = countSumInRow(matrix, 1);
+        //System.out.println(sum);
 
-        System.out.println();
-        System.out.println(k);
-        System.out.println(sum);
-    }
-
-    private static int count(int[][] matrix, int max) {
-        int res = 0;
-        int s = 0;
-
-        // оператор break с меткой
-        OUTER:
-        for (int i = 0; i < matrix.length; i++) {
-            int[] row = matrix[i];
-            for (int j = 0; j < matrix[i].length; j++) {
-                res++;
-                s += row[j];
-                System.out.print(s);
-                if (s > max) {
-                    break OUTER;
-                }
-            }
-        }
-        return res;
+        countSumInRows(matrix);
     }
 
     private static int countSumInRow(int[][] matrix, int rowNum) {
@@ -49,5 +27,21 @@ public class MatrixProcessing {
             }
         }
         return sum;
+    }
+
+    private static void countSumInRows(int[][] matrix) {
+        OUTER:
+        for (int i = 0; i < matrix.length; i++) {
+            int[] row = matrix[i];
+            int sum = 0;
+
+            for (int j = 0; j < row.length; j++) {
+                if (row[j] < 0) {
+                    continue OUTER;
+                }
+                sum += row[j];
+            }
+            System.out.println("Row " + i + " - sum: " + sum);
+        }
     }
 }
