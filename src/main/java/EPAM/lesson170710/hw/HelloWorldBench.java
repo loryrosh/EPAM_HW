dc730033dc56b5fd7297a7542b06765f0086e511
loryrosh <sasharo@mail.ru>
2017-07-10 23:49:58 +0300

    Trying out to connect JMH benchmark  (http://nitschinger.at/Using-JMH-for-Java-Microbenchmarking/)

---
+++ src/main/java/EPAM/lesson170710/hw/HelloWorldBench.java
@@ -0,0 +1,11 @@
+package EPAM.lesson170710.hw;
+
+import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
+
+public class HelloWorldBench {
+
+    @GenerateMicroBenchmark
+    public void helloWorld() {
+
+    }
+}
---
+++ src/main/java/EPAM/lesson170710/hw/JMHTest.java
@@ -0,0 +1,9 @@
+package EPAM.lesson170710.hw;
+
+import org.openjdk.jmh.Main;
+
+public class JMHTest {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
