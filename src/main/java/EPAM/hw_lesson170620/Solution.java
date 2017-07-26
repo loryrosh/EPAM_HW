9854ddfc56be70c5d99aaa9bd8f2fc85d4cb07a2
loryrosh <sasharo@mail.ru>
2017-07-26 21:47:00 +0300

    HackerRank Hourglass problem

--- src/main/java/EPAM/hw_lesson170620/Solution.java
+++ src/main/java/EPAM/hw_lesson170620/Solution.java
@@ -5,9 +5,39 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
+        Scanner in = new Scanner(System.in);
+        /*
+        int arr[][] = new int[][]{
+                {1, 1, 1, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0},
+                {1, 1, 1, 0, 0, 0},
+                {0, 0, 2, 4, 4, 0},
+                {0, 0, 0, 2, 0, 0},
+                {0, 0, 1, 2, 4, 0}
+        };
+        */
 
-        
+        int arr[][] = new int[][]{
+                {-1, -1, 0, -9, -2, -2},
+                {-2, -1, -6, -8, -2, -5},
+                {-1, -1, -1, -2, -3, -4},
+                {-1, -9, -2, -4, -4, -5},
+                {-7, -3, -3, -2, -9, -9},
+                {-1, -3, -1, -2, -4, -5}
+        };
+
+        int maxSum = -1000;
+        for (int i = 0; i < arr.length - 2; i++) {
+            int sum = 0;
+            for (int j = 0; j < arr[0].length - 2; j++) {
+                sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] +
+                        arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+
+                if (maxSum < sum) {
+                    maxSum = sum;
+                }
+            }
+        }
+        System.out.println(maxSum);
     }
 }
